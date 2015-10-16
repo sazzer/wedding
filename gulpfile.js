49b5314011cc765babfd44ccab5c31ffdcf1c5de
Graham Cox <graham@grahamcox.co.uk>
2015-10-16 14:18:46 +0100

    Dropped in Webpack for building Javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var webpack = require('gulp-webpack');
 
 gulp.task('webserver', function() {
     connect.server({
@@ -18,6 +19,18 @@ gulp.task('html', function() {
         .pipe(connect.reload());
 });
 
+gulp.task('javascript', function() {
+    gulp.src('src/js/main.js')
+        .pipe(sourcemaps.init())
+            .pipe(webpack({
+                output: {
+                    filename: '[name].js'
+                }
+            }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('target/js'))
+        .pipe(connect.reload());
+});
 gulp.task('sass', function() {
     gulp.src('src/scss/*.scss')
         .pipe(sourcemaps.init())
@@ -31,7 +44,8 @@ gulp.task('sass', function() {
 gulp.task('watch', function() {
     gulp.watch(['./src/scss/*.scss'], ['sass']);
     gulp.watch(['./src/html/*.html'], ['html']);
+    gulp.watch(['./src/js/*.js'], ['javascript']);
 });        
 
-gulp.task('build', ['sass', 'html']);
+gulp.task('build', ['sass', 'html', 'javascript']);
 gulp.task('run', ['build', 'webserver', 'watch']);
