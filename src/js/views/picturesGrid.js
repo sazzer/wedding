a1d0bac4d7065183f47aa02c35efcb904e4275d8
Graham Cox <graham@grahamcox.co.uk>
2015-10-17 16:38:37 +0100

    Separate React component for the individual thumbnails

--- src/js/views/picturesGrid.js
+++ src/js/views/picturesGrid.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Reflux from 'reflux';
 import {PicturesStore} from '../stores/picturesStore';
+import {Thumbnail} from './thumbnail';
 
 export const PicturesGrid = React.createClass({
     mixins: [
@@ -8,28 +9,12 @@ export const PicturesGrid = React.createClass({
     ],
     render: function() {
         const pictures = this.state.pictures;
-        const picturesList = pictures.map((p) => {
-            const url = `images/thumbnails/${p.file}`;
-
-            return <div className="col-lg-2 col-md-2 col-xs-2 thumb" key={p.file}>
-                <a className="thumbnail" href="#" title={p.title} onClick={this.onClick}>
-                    <img className="img-responsive" src={url} alt={p.title} />
-                </a>
-            </div>;
-        });
+        const picturesList = pictures.map((p) => <Thumbnail key={p.file} {...p} />);
 
         return <div className="container">
             <div className="row">
                 {picturesList}
             </div>
         </div>;
-    },
-
-    /**
-     * Handler for when a thumbnail is clicked
-     * @param e The even
-     */
-    onClick: function(e) {
-        console.log("Clicked thumbnail");
     }
 });
---
+++ src/js/views/thumbnail.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const Thumbnail = React.createClass({
+    render: function() {
+        const {file, title} = this.props;
+        const url = `images/thumbnails/${file}`;
+
+        return <div className="col-lg-2 col-md-2 col-xs-2 thumb" key={file}>
+            <a className="thumbnail" href="#" title={title} onClick={this.onClick}>
+                <img className="img-responsive" src={url} alt={title} />
+            </a>
+        </div>;
+    },
+
+    /**
+     * Handler for when a thumbnail is clicked
+     * @param e The even
+     */
+    onClick: function(e) {
+        console.log(`Clicked thumbnail: ${this.props.file}`);
+    }
+});
