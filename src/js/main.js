d1d496633ddcb151c088de4c0494cf165050b0cf
Graham Cox <graham@grahamcox.co.uk>
2015-10-17 11:25:17 +0100

    Actually render a real React page as the homepage. Rather boring but something.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
-import {name} from './second';
-
-var React = require('react');
-var ReactDOM = require('react-dom');
+import ReactDOM from 'react-dom';
+import React from 'react';
+import {HomePage} from './views/home';
 
 ReactDOM.render(
-  <h1>Hello, {name()}</h1>,
-  document.getElementById('example')
+    <HomePage />,
+    document.body
 );
---
+++ src/js/views/home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export const HomePage = React.createClass({
+    render: function() {
+        return <div>Hello, World</div>;
+    }
+});
